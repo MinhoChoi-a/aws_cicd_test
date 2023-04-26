2fef36fea9a64827d8e510c67639ac0e853262c0
minhochoi-a <minhocalgary@gmail.com>
2023-04-25 20:30:37 -0600

    init

---
+++ init.sh
@@ -0,0 +1,29 @@
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+curl -sSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+sudo curl -o /usr/local/bin/ecs-cli https://amazon-ecs-cli.s3.amazonaws.com/ecs-cli-linux-amd64-latest
+sudo chmod +x /usr/local/bin/ecs-cli
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo usermod -a -G docker ubuntu
+sudo mkdir -p /etc/ecs
+sudo touch /etc/ecs/ecs.config
+echo "ECS_CLUSTER=<your_cluster_name>" | sudo tee /etc/ecs/ecs.config
+curl -o /tmp/ecs-agent.tar https://amazon-ecs-agent.s3.amazonaws.com/ecs-agent-latest.tar.gz
+sudo tar xzf /tmp/ecs-agent.tar -C /opt
+sudo mkdir -p /var/log/ecs /var/lib/ecs/data /var/lib/ecs/scratch
+sudo docker volume create --name ecs-vol
+sudo docker run --name ecs-agent \
+  --detach=true \
+  --restart=on-failure:10 \
+  --volume=/var/run/docker.sock:/var/run/docker.sock \
+  --volume=/var/log/ecs:/log \
+  --volume=/var/lib/ecs/data:/data \
+  --volume=/var/lib/ecs/scratch:/scratch \
+  --volume=ecs-vol:/var/lib/ecs/volumes \
+  --publish=127.0.0.1:51678:51678 \
+  --env-file=/etc/ecs/ecs.config \
+  amazon/amazon-ecs-agent:latest
